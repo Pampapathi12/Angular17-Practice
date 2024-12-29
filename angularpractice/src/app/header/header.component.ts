45869512f579300d62050c9cf2368bff6e6fb02d
Pampapathi K <pampapathi@kalajitha.com>
2024-12-29 19:30:02 +0530

    4.View Template of Component

--- angularpractice/src/app/header/header.component.ts
+++ angularpractice/src/app/header/header.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core'; // all the angular directves and core
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  template: '<h1>ekart</h1>',
+  // template: '<h1>ekart</h1>', // if less line of code the use the template
+  templateUrl: './header.component.html', // nned specify the path of the html
+ 
   styleUrls: ['./header.component.css']
 }) // meta data object and its property and value
 // selector :- property value : 'app-header'
