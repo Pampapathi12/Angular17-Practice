7bc7dd5f3bdeeef6313fbaf059d969379916eeb8
Pampapathi K <pampapathi@kalajitha.com>
2024-12-29 20:02:48 +0530

    5. Styling View Template

--- angularpractice/src/app/header/header.component.ts
+++ angularpractice/src/app/header/header.component.ts
@@ -5,7 +5,8 @@ import { Component } from '@angular/core'; // all the angular directves and core
   // template: '<h1>ekart</h1>', // if less line of code the use the template
   templateUrl: './header.component.html', // nned specify the path of the html
  
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  // styles: ['#{text-decoration: none; margin:0 10px;}', 'button{padding: 10px 20px}', '.ekart--header{width: 100%; height:70px}', '#id{}']
 }) // meta data object and its property and value
 // selector :- property value : 'app-header'
 export class HeaderComponent {
