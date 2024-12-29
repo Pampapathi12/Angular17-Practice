93dae715ee659aedd03cafd3e087c5ce3c5857b5
Pampapathi K <pampapathi@kalajitha.com>
2024-12-29 16:31:19 +0530

    2. Boostraping Angular Application

--- angularpractice/src/app/app.component.ts
+++ angularpractice/src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
+})// meta data object, select property is mandatory and template
 export class AppComponent {
   title = 'angularpractice';
 }
--- angularpractice/src/app/app.module.ts
+++ angularpractice/src/app/app.module.ts
@@ -6,11 +6,11 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent
-  ],
+  ],// declare the compoents
   imports: [
     BrowserModule
-  ],
+  ], // external module for the dependenciews
   providers: [],
-  bootstrap: [AppComponent]
-})
+  bootstrap: [AppComponent]// that will get loaded firstly
+}) // Meta data object
 export class AppModule { }
