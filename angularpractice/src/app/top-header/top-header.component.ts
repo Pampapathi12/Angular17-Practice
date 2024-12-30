aab3bc274b1e10e3e46071adb1d212c8041e01a3
Pampapathi K <pampapathi@kalajitha.com>
2024-12-30 15:17:50 +0530

    9.Types of Component Selector

--- angularpractice/src/app/top-header/top-header.component.ts
+++ angularpractice/src/app/top-header/top-header.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'top-header',
+  selector: 'top-header', // html element
+  // selector: '[top-header]', // html aatribute
+  // selector: '.top-header', // css class or id , in case of angular directive
+  // selector: '#top-header',
   templateUrl: './top-header.component.html',
   styleUrls: ['./top-header.component.css']
 })
