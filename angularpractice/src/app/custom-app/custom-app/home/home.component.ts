0bbbf5148446983e02ad1d401924209184bbd487
Pampapathi K <pampapathi@kalajitha.com>
2025-01-14 17:25:17 +0530

    28. viewchildre in angular

--- angularpractice/src/app/custom-app/custom-app/home/home.component.ts
+++ angularpractice/src/app/custom-app/custom-app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChildren, ViewChild, QueryList } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+
+// retunr singel elemtn and first elemnt the which matches the selector
+//  @ViewChild('inputEl') inputElements : ElementRef;
+fullName: string = '';
+
+@ViewChildren('inputEl')inputElements :  QueryList< ElementRef>
+
+
+
+
+  show(){
+
+    let name = ''
+
+    // console.log(this.inputElements.nativeElement);
+   this.inputElements.forEach((el) =>{
+      console.log(el.nativeElement.value)
+
+      name += el.nativeElement.value + ' '
+    })
+    this.fullName = name.trim()
+
+  }
 
 }
