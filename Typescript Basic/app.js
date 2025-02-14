71145a89ce475ec8cbea4a94f5b89ef9f8e51d7e
Pampapathi K <pampapathi@kalajitha.com>
2025-02-14 23:12:26 +0530

    04 Datatypes in TypeScript

--- Typescript Basic/app.js
+++ Typescript Basic/app.js
@@ -1,8 +1,28 @@
 // console.log('helloworld')
 //03 Variables in TypeScript 
-var num = 100;
-num = 1000; // change the value to 
+/* let num = 100;
+
+ num = 1000;// change the value to
 // num = 'pampapathi'//Type 'string' is not assignable to type 'number', strongly typed
-var str = 'Hello world'; // initialize must be const
+
+const str = 'Hello world'// initialize must be const
 // str = "pampa";
-console.log(num, str);
+
+
+console.log(num,str) */
+//04 Datatypes in TypeScript 
+/* String Number Boolean */
+var str2 = 'pampapathi';
+var str1 = 'pampapathi';
+var str3 = "pampapathhi  ".concat(str1); // back ticks / for the multipl lines
+console.log(str2, str1, str3);
+// number
+var num = 12; // saveed in the memory floating point number 12.0
+var pi = 3.12;
+// boolean
+var isEligible = true;
+var isEqual = false; // truthy and falsey values, 0,empty string, null, undefined are falsy values
+console.log(Boolean(0));
+console.log(Boolean(100), Boolean(''), Boolean(null), Boolean('hello'));
+var isGreater = 10 > 15;
+console.log(isGreater);
--- Typescript Basic/app.ts
+++ Typescript Basic/app.ts
@@ -2,7 +2,7 @@
 
 //03 Variables in TypeScript 
 
-let num = 100;
+/* let num = 100;
 
  num = 1000;// change the value to 
 // num = 'pampapathi'//Type 'string' is not assignable to type 'number', strongly typed
@@ -11,4 +11,32 @@ const str = 'Hello world'// initialize must be const
 // str = "pampa";
 
 
-console.log(num,str)
+console.log(num,str) */
+
+//04 Datatypes in TypeScript 
+/* String Number Boolean */
+const str2 = 'pampapathi';
+const str1: string = 'pampapathi';
+const str3 = `pampapathhi  ${str1}`;// back ticks / for the multipl lines
+
+console.log(str2,str1,str3)
+
+// number
+
+let num = 12;// saveed in the memory floating point number 12.0
+
+const pi = 3.12;
+
+// boolean
+
+let isEligible = true;
+let isEqual = false;// truthy and falsey values, 0,empty string, null, undefined are falsy values
+
+console.log(Boolean(0));
+
+console.log(Boolean(100), Boolean(''), Boolean(null), Boolean('hello'));
+
+let isGreater = 10 > 15;// result as boolean type based on the condition
+console.log(isGreater)
+
+
