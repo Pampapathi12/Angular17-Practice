3093d65c938c6b415945c13435883843cd9ee96b
Pampapathi K <pampapathi@kalajitha.com>
2025-02-14 22:31:52 +0530

    4 variable

--- Typescript Basic/app.js
+++ Typescript Basic/app.js
@@ -1,1 +1,8 @@
-console.log('helloworld');
+// console.log('helloworld')
+//03 Variables in TypeScript 
+var num = 100;
+num = 1000; // change the value to 
+// num = 'pampapathi'//Type 'string' is not assignable to type 'number', strongly typed
+var str = 'Hello world'; // initialize must be const
+// str = "pampa";
+console.log(num, str);
--- Typescript Basic/app.ts
+++ Typescript Basic/app.ts
@@ -1,1 +1,14 @@
-console.log('helloworld')
+// console.log('helloworld')
+
+//03 Variables in TypeScript 
+
+let num = 100;
+
+ num = 1000;// change the value to 
+// num = 'pampapathi'//Type 'string' is not assignable to type 'number', strongly typed
+
+const str = 'Hello world'// initialize must be const
+// str = "pampa";
+
+
+console.log(num,str)
