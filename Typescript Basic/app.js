e6abd0ed59591371c72276daa4b1a36ef8cb4bbe
Pampapathi K <pampapathi@kalajitha.com>
2025-02-19 12:50:21 +0530

    05 Type Assignment & Type Inference

--- Typescript Basic/app.js
+++ Typescript Basic/app.js
@@ -12,17 +12,41 @@ const str = 'Hello world'// initialize must be const
 console.log(num,str) */
 //04 Datatypes in TypeScript 
 /* String Number Boolean */
-var str2 = 'pampapathi';
-var str1 = 'pampapathi';
-var str3 = "pampapathhi  ".concat(str1); // back ticks / for the multipl lines
-console.log(str2, str1, str3);
+/* const str2 = 'pampapathi';
+const str1: string = 'pampapathi';
+const str3 = `pampapathhi  ${str1}`;// back ticks / for the multipl lines
+
+console.log(str2,str1,str3)
+
 // number
-var num = 12; // saveed in the memory floating point number 12.0
-var pi = 3.12;
+
+let num = 12;// saveed in the memory floating point number 12.0
+
+const pi = 3.12;
+
 // boolean
-var isEligible = true;
-var isEqual = false; // truthy and falsey values, 0,empty string, null, undefined are falsy values
+
+let isEligible = true;
+let isEqual = false;// truthy and falsey values, 0,empty string, null, undefined are falsy values
+
 console.log(Boolean(0));
+
 console.log(Boolean(100), Boolean(''), Boolean(null), Boolean('hello'));
-var isGreater = 10 > 15;
-console.log(isGreater);
+
+let isGreater = 10 > 15;// result as boolean type based on the condition
+console.log(isGreater) */
+/* 05 Type Assignment & Type Inference */
+function sum(num1, num2, isPrint, msg) {
+    if (isPrint) {
+        var s = num1 + num2;
+        console.log(msg + '' + s);
+    }
+    return num1 + num2;
+}
+var n1 = 10;
+var n2 = '20'; // type inference, which we have initilize the value
+//n2 = 'pampa'// error Type 'string' is not assignable to type 'number'.
+console.log(sum(12, 13, true, "sum is ="));
+console.log(n1, n2);
+//console.log(sum('12',13))// string value and number will continate
+//Argument of type 'string' is not assignable to parameter of type 'number
--- Typescript Basic/app.ts
+++ Typescript Basic/app.ts
@@ -15,7 +15,7 @@ console.log(num,str) */
 
 //04 Datatypes in TypeScript 
 /* String Number Boolean */
-const str2 = 'pampapathi';
+/* const str2 = 'pampapathi';
 const str1: string = 'pampapathi';
 const str3 = `pampapathhi  ${str1}`;// back ticks / for the multipl lines
 
@@ -37,6 +37,27 @@ console.log(Boolean(0));
 console.log(Boolean(100), Boolean(''), Boolean(null), Boolean('hello'));
 
 let isGreater = 10 > 15;// result as boolean type based on the condition
-console.log(isGreater)
+console.log(isGreater) */
 
+/* 05 Type Assignment & Type Inference */
+
+
+function sum(num1: number, num2:number, isPrint:boolean, msg:string){
+    if(isPrint){
+        let s = num1 + num2;
+        console.log(msg+'' +s)
+    }
+    return num1 + num2;
+}
+
+let n1 = 10;
+let n2 = '20';// type inference, which we have initilize the value
+//n2 = 'pampa'// error Type 'string' is not assignable to type 'number'.
+
+
+
+console.log(sum(12,13,true, "sum is ="));
+console.log(n1,n2);
+//console.log(sum('12',13))// string value and number will continate
+//Argument of type 'string' is not assignable to parameter of type 'number
 
